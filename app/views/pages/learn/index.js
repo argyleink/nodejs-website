662d40d46724c086bcbe545fd9ac8e2996aeb060
Adam Argyle <atom@argyleink.com>
2019-02-01 09:18:09 -0800

    learn page doin alright (needs mobile)

--- app/views/pages/learn/index.js
+++ app/views/pages/learn/index.js
@@ -1,2 +1,3 @@
 import 'ragrid'
-import { body, header } from '~/css/elements'
+import { body, header, article, aside, hr, ol } from '~/css/elements'
+import { guide_list, guide_hero, repl, footer } from '~/css/components'
